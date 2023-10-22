529b084e36286a12f8dfa1c3df41cecfe66f73df
kikok <s1090374@gm.pu.edu.tw>
2023-10-22 17:06:44 +0800

    加入專案檔案。

---
+++ bigger/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace bigger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("");
+            string[] str = Console.ReadLine().Split(' ');
+            int num1, num2, num3;
+
+            num1 = int.Parse(str[0]);
+            num2 = int.Parse(str[1]);
+            num3 = int.Parse(str[2]);
+
+            // 使用排序函數將數字排序
+            int[] numbers = { num1, num2, num3 };
+            Array.Sort(numbers);
+
+            // 顯示排序後的數字
+            Console.Write("");
+            foreach (int num in numbers)
+            {
+                Console.Write(num+" ");
+            }
+        }
+    }
+}
